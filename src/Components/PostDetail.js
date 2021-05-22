4eb8ebb35015ac26fbc8ec340af53729d7e2c137
Ebinu-S <ebinusuneer2nd@gmail.com>
2021-05-22 23:19:47 +0530

    initialised fullscreen in postdetails and styling

--- src/Components/PostDetail.js
+++ src/Components/PostDetail.js
@@ -7,14 +7,18 @@ import { Avatar } from "@material-ui/core";
 import Tooltip from '@material-ui/core/Tooltip';
 import Modal from '@material-ui/core/Modal';
 import DeletePost from './DeletePost.js';
+import {FullScreen, useFullScreenHandle} from 'react-full-screen';
 
 const PostDetail = () => {
     const {postid} = useParams(); 
     const [currentPost, setCurrentPost] = useState({});
     const [comments, setComments] = useState([]);
     const [deleteOpen,setDeleteOpen] = useState(false);
+    const [menuToggle,setMenuToggle] = useState(false);
     const {posts} = useFetch();
     const user = auth.currentUser;
+
+    const handle = useFullScreenHandle();
 
     useEffect(() => {
         if(posts[0]){
@@ -48,52 +52,56 @@ const PostDetail = () => {
         {/* add view in full screen */}
         {user ? (currentPost && (
         <span className="detail__container">
-            <div className='detail__header'>
-                <span>
-                    <Avatar src='static/images/avatar/1.jpg' alt={currentPost.username} />
-                    <strong><Link to={`/profile/${currentPost.username}`}>{currentPost.username}</Link></strong>
-                </span>
-                <span>
-                    <button ><i class="fas fa-ellipsis-v"></i></button>
-                    <div className="details__menu">
-                        {user && user.displayName === currentPost.username? (
-                            <Tooltip title="Delete this post">
-                                <button onClick={() => setDeleteOpen(true)}>
-                                <i class="fas fa-trash-alt"></i></button>
-                            </Tooltip>):('')
-                }
-                    </div>
-                </span>
-            </div>
-            <img src={currentPost.imageUrl} className='detail__image'/>
-            <div className='detail__captionContainer'>
-                <p>
-                    <strong><Link to={`/profile/${currentPost.username}`}>{currentPost.username}</Link></strong>
-                    {currentPost.caption}
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis neque sit amet diam faucibus gravida. Nulla blandit dui sodales metus facilisis, ac ultricies mauris congue. Nulla a dolor id lacus vestibulum tempor vel vitae tellus. In commodo iaculis urna, eu tempus erat malesuada cursus. Pellentesque tincidunt mattis nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie erat neque, id bibendum velit sollicitudin eget.
-                </p>
-            </div>
-            <div className='detail__commentContainer'>{comments.map(cmt => (
-                <div>
-                    <Avatar src='static/images/avatar/1.jpg' alt={cmt.displayName}/>
-                    <span><strong><Link to={`/profile/${cmt.displayName}`}>{cmt.displayName}</Link></strong>{cmt.text}
+            <FullScreen  handle={handle}> 
+                <div className='detail__header'>
+                    <span>
+                        <Avatar src='static/images/avatar/1.jpg' alt={currentPost.username} />
+                        <strong><Link to={`/profile/${currentPost.username}`}>{currentPost.username}</Link></strong>
+                    </span>
+                    <span>
+                        <Tooltip title="View FullScreen"><button onClick={handle.enter}><i class="fas fa-expand-alt"></i></button></Tooltip>
+                        <button onClick={() => setMenuToggle(!menuToggle)}><i class="fas fa-ellipsis-v"></i></button>
+                        <div className={`details__menu ${menuToggle? '' : 'hidden'}`}>
+                            {user && user.displayName === currentPost.username? (
+                                <Tooltip title="Delete this post">
+                                    <button className="dlt_btn" onClick={() => setDeleteOpen(true)}>
+                                    <i className="fas fa-trash-alt"></i></button>
+                                </Tooltip>):('')
+                                // add edit here?
+                            }
+                        </div>
                     </span>
                 </div>
-                ))}
-            <span className='detail__commentInput'>
-            <InputComment postId={postid}/>
-            </span>
-            </div>
-        </span> 
-        )):(
-            <h1>Signup</h1>
-        )}
+            <img src={currentPost.imageUrl} className='detail__image'/>
+                <div className='detail__captionContainer'>
+                    <p>
+                        <strong><Link to={`/profile/${currentPost.username}`}>{currentPost.username}</Link></strong>
+                        {currentPost.caption}
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sagittis neque sit amet diam faucibus gravida. Nulla blandit dui sodales metus facilisis, ac ultricies mauris congue. Nulla a dolor id lacus vestibulum tempor vel vitae tellus. In commodo iaculis urna, eu tempus erat malesuada cursus. Pellentesque tincidunt mattis nibh. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi molestie erat neque, id bibendum velit sollicitudin eget.
+                    </p>
+                </div>
+                <div className='detail__commentContainer'>{comments.map(cmt => (
+                    <div>
+                        <Avatar src='static/images/avatar/1.jpg' alt={cmt.displayName}/>
+                        <span><strong><Link to={`/profile/${cmt.displayName}`}>{cmt.displayName}</Link></strong>{cmt.text}
+                        </span>
+                    </div>
+                    ))}
+                <span className='detail__commentInput'>
+                <InputComment postId={postid}/>
+                </span>
+                </div>
+            </FullScreen>
+            </span> 
+            )):(
+                <h1>Signup</h1>
+            )}
 
-        <Modal 
-        open={deleteOpen}
-        onClose={() => setDeleteOpen(false)}>
-            <DeletePost setDeleteOpen={setDeleteOpen} currentId={postid} username={currentPost.username}/>
-        </Modal>
+            <Modal 
+            open={deleteOpen}
+            onClose={() => setDeleteOpen(false)}>
+                <DeletePost setDeleteOpen={setDeleteOpen} currentId={postid} username={currentPost.username}/>
+            </Modal>
     </div> );
 }
  
